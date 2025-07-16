cee6357de2eef5eef657c69a3b1f000d1127a58f
t-powers <127955989+t-powers@users.noreply.github.com>
2025-07-15 20:49:43 -0400

    Mistake cleanup

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
-const firstInput = 5;
-const operatorBtn = {add: '+', subtract: '-', multiply: '*', divide: '/'};
-const secondInput = 50;
+let firstInput = '';
+let operatorBtn = {add: '+', subtract: '-', multiply: '*', divide: '/'};
+let secondInput = '';
+
+const clickDigits = document.getElementById('[digits]');
+const output = document.getElementById('viewCalculation');
 
 
 const calculateInputs = (firstInput, secondInput) => {
@@ -15,8 +18,3 @@ const calculateInputs = (firstInput, secondInput) => {
 const operate = (firstInput, secondInput) => {
     return `${firstInput} ${operatorBtn.add} ${secondInput} = `;
 };
-
-const displayResults = document.getElementById('viewCalculation');
-displayResults.textContent = operate(firstInput, secondInput) + calculateInputs(firstInput, secondInput);
-
-
