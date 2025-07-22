efb23200caeae667ce1c6570eb8fd2048de00709
t-powers <127955989+t-powers@users.noreply.github.com>
2025-07-21 21:07:13 -0400

    Updated function comments

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ let firstInput = '';
 let operatorBtn = {add: '+', subtract: '-', multiply: '*', divide: '/'};
 let secondInput = '';
 
+// snagged digit button elements
 const clickDigits = document.querySelectorAll('.digits');
+
+// got calculator's screen
 const viewCalculation = document.getElementById('viewCalculation');
 
 
+// added adding function to test addition :+)
 const calculateInputs = (firstInput, secondInput) => {
     return add(firstInput, secondInput);
 };
+
+// formatted calculation input/outputs onto viewCalculation
 const operate = () => {
     return `${firstInput} ${operatorBtn.add} ${secondInput} = ` + add(firstInput, secondInput);
 };
