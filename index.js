d0bac2bf2cd6ffcb762ffa1a019aa75c3a7ca40d
t-powers <127955989+t-powers@users.noreply.github.com>
2025-07-22 17:47:52 -0400

    Removed to redo.

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
-// #2 setup variables for numbers and operator
-let firstInput = '';
+// #2 variables for numbers and operator
+let firstInput = 0;
 let operatorBtn = {add: '+', subtract: '-', multiply: '*', divide: '/'};
-let secondInput = '';
+let secondInput = 0;
 
 // snagged digit button elements
 const clickDigits = document.querySelectorAll('.digits');
-
+// grabbed calculator functions
+const clickFunctions = document.querySelectorAll('.function');
 // got calculator's screen
 const viewCalculation = document.getElementById('viewCalculation');
 
-// #3, used add function to test
-const operate = () => {
-    return `${firstInput} ${operatorBtn.add} ${secondInput} = ` + add(firstInput, secondInput);
-};
+
+
+
 
 
 
