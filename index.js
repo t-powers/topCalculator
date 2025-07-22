bb9eb77f1653336cd7f4e8dfe960f721383dd434
t-powers <127955989+t-powers@users.noreply.github.com>
2025-07-22 14:33:50 -0400

    Updated comments, removed eventlistener for rebuild

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// #1 added operand functions
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
+// #2 setup variables for numbers and operator
 let firstInput = '';
 let operatorBtn = {add: '+', subtract: '-', multiply: '*', divide: '/'};
 let secondInput = '';
@@ -13,18 +15,7 @@ const clickDigits = document.querySelectorAll('.digits');
 // got calculator's screen
 const viewCalculation = document.getElementById('viewCalculation');
 
-clickDigits.forEach(digit => {
-    digit.addEventListener('click', (e) => {
-        const buttonPress = e.target.textContent;
-        viewCalculation.textContent = buttonPress;
-    });
-});
-// added adding function to test addition :+)
-const calculateInputs = (firstInput, secondInput) => {
-    return add(firstInput, secondInput);
-};
-
-// formatted calculation input/outputs onto viewCalculation
+// #3, used add function to test
 const operate = () => {
     return `${firstInput} ${operatorBtn.add} ${secondInput} = ` + add(firstInput, secondInput);
 };
