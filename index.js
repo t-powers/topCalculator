9c3d1429f17eaf0aec220730ebe7d919cf9d6768
t-powers <127955989+t-powers@users.noreply.github.com>
2025-07-22 21:18:41 -0400

    Put digits and symbols into view

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
 // #2 variables for numbers and operator
-let firstInput = 0;
+let firstInput = '';
 let operatorBtn = {add: '+', subtract: '-', multiply: '*', divide: '/'};
-let secondInput = 0;
+let secondInput = '';
 
 // snagged digit button elements
 const clickDigits = document.querySelectorAll('.digits');
 // grabbed calculator functions
-const clickFunctions = document.querySelectorAll('.function');
+const clickFunctions = document.querySelectorAll('.functions');
 // got calculator's screen
 const viewCalculation = document.getElementById('viewCalculation');
+
+// #3
+
+clickDigits.forEach(digit => {
+    digit.addEventListener('click', (e) => {
+        const targetValue = e.target.textContent;
+        viewCalculation.textContent += targetValue;
+
+    });
+});
+
+clickFunctions.forEach(symbols => {
+    symbols.addEventListener('click', (e) => {
+        const targetValue = e.target.textContent;
+        viewCalculation.textContent += targetValue;
+
+    });
+});
 
 
 
