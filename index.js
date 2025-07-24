00690a31059cc3aefc4a9ecb70dba8d8a51f4bce
t-powers <127955989+t-powers@users.noreply.github.com>
2025-07-24 19:16:07 -0400

    Placed operator functions into object

---
+++ index.js
@@ -0,0 +1,21 @@
+let firstInput = 1;
+let secondInput = 2;
+
+
+const operatorFunctions = {
+    '+': (firstInput, secondInput) => {
+        return firstInput + secondInput;
+    },
+    '-': (firstInput, secondInput) => {
+        return firstInput - secondInput;
+    },
+    '*': (firstInput, secondInput) => {
+        return firstInput * secondInput;
+    },
+    '/': (firstInput, secondInput) => {
+        return firstInput / secondInput;
+    }
+};
+
+
+
