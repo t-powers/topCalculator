15720d6d909f8f6961de84c62c07595b40e1ac0d
t-powers <127955989+t-powers@users.noreply.github.com>
2025-07-25 14:33:38 -0400

    Iterated and added e listener to test in DOM

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let firstOperand = '';
 let secondOperand = '';
 let inputOperator = '';
 const viewDisplay = document.getElementById('viewCalculation');
+const digits = document.querySelectorAll('.digits');
+
+digits.forEach(digit => {
+    digit.addEventListener('click', (e) => {
+        console.log(e.target.textContent);
+    });
+});
 
 
 const operatorFunctions = {
@@ -18,6 +25,4 @@ const operatorFunctions = {
         return firstInput / secondInput;
     }
 };
-
-
 
