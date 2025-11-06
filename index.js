189a5a766d91f3ff571dee42e9b46c4f8b5e9987
t-powers <127955989+t-powers@users.noreply.github.com>
2025-11-05 19:00:54 -0500

    Clear all func added, ac, negative, percent were added.

--- index.js
+++ index.js
@@ -8,4 +8,66 @@ const buttonValues = [
 const rightSymbols = ['÷', '×', '-', '+', '='];
 const topSymbols = ['AC', '+/-', '%'];
 
+const display = document.getElementById('display');
 
+let a = 0;
+let operator = null;
+let b = null;
+
+for (let i = 0; i < buttonValues.length; i++) {
+    const button = document.createElement('button');
+    const value = buttonValues[i];
+    button.innerText = value;
+
+
+    if (value == 0) {
+        button.style.width = '200px';
+        button.style.gridColumn = 'span 2';
+    }
+
+    button.addEventListener('click', () => {
+        if (rightSymbols.includes(value)) {
+
+        } else if (topSymbols.includes(value)) {
+            if (value == 'AC') {
+                clearAll();
+                display.value = '';
+
+            } else if (value == '+/-') {
+                if (display.value != '' && display.value != '0') {
+                    if (display.value[0] == '-') {
+                        display.value = display.value.slice(1);
+                    } else {
+                        display.value = '-' + display.value;
+                    }
+                }
+
+
+            } else if (value == '%') {
+                display.value = Number(display.value) / 100;
+            }
+
+        } else {
+            if (value == '.') {
+                if (display.value != '' && !display.value.includes(value)) {
+                    display.value += value;
+                }
+
+            } else if (display.value == '0') {
+                display.value = value;
+            } else {
+                display.value += value;
+            }
+        }
+    });
+
+    function clearAll() {
+        let a = 0;
+        let operator = null;
+        let b = null;
+
+    }
+
+    document.getElementById('buttons').appendChild(button);
+
+}
